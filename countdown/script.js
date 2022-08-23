3f4b2ff996fd0c155f0ebb1f14443eb71e2327b0
mutharasi <rithikakumar01@gmail.com>
2022-08-23 15:40:32 +0530

    first commit

---
+++ countdown/script.js
@@ -0,0 +1,31 @@
+const countdownDay = new Date("Aug 30,2022 00:00:00").getTime();
+const items = document.querySelectorAll(".deadline .count");
+const page=document.querySelector('.deadlineInfo')
+
+
+var countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countdownDay - now;
+  
+  if(distance<0){
+    clearInterval(countdown)
+    page.innerHTML=`<h1>Sorry,this giveaway expired!</h1>`
+}
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var sec = Math.floor((distance % (1000 * 60)) / 1000);
+  const value = [days, hours, mins, sec];
+  function format(items) {
+    if (items < 10) {
+      return `0${items}`;
+    }
+    return items;
+  }
+
+  items.forEach(function (items, index) {
+    items.innerHTML = format(value[index]);
+  });
+
+}, 1000);
+
